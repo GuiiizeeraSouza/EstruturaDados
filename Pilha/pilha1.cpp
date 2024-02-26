b15d6d04f96abd1a1e3d378a509fee9f5c536c8b
GuiiizeeraSouza <guilhermefsouzaaa@gmail.com>
2024-02-26 19:44:53 -0300

    feat(Pilha): Exemplo de pilha

---
+++ Pilha/pilha1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct nodo {
+    int valor;
+    struct nodo *prox;
+}Pilha; 
+
+Pilha *inserir(int valor, Pilha *topo) {
+    //Alocar memória
+    Pilha *novo = (Pilha *)malloc(sizeof(Pilha));
+
+    //depositar valores dos parametros na estrutura criada
+    novo->valor = valor;
+
+    //engatar
+    novo->prox = topo;
+    
+    //Retornar o novo topo
+    return topo;
+}
+
+void Imprimir(Pilha *topo){
+    if(!topo) return;
+    for (Pilha *i = topo; i !=NULL; i = i->prox) [
+        
+    ]
+}
+
+int main(){
+    Pilha *pilhaNumeros = NULL;
+
+    pilhaNumeros = inserir(10, pilhaNumeros);
+    pilhaNumeros = inserir(1, pilhaNumeros);
+    pilhaNumeros = inserir(100, pilhaNumeros);
+
+    // pilhaNumeros = remover(pilhaNumeros);
+
+    Imprimir(pilhaNumeros);    
+}
